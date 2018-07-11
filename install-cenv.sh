0aed9e4ce330991294069c408acb341e455cf76d
Tai Kedzierski <dch.tai@gmail.com>
2018-07-11 11:36:03 +0100

    Added busybox injection support

--- install-cenv.sh
+++ install-cenv.sh
@@ -10,4 +10,13 @@ cd "$(dirname "$0")"
 
 cp cenv "$bindir/cenv"
 
-ln -s "$bindir/cenv" "$bindir/cenvx"
+extra_targets=(cenvx cenvb)
+
+for t in "${extra_targets[@]}"; do
+    [[ ! -e "$bindir/$t" ]] || continue
+    ( set -x
+    ln -s "$bindir/cenv" "$bindir/$t"
+    )
+done
+
+echo "Installed commands: cenv ${extra_targets[*]}"
